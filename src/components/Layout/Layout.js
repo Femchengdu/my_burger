587371b6982809c1dfdfcf21d1f4465e4c3bdc80
vagrant <vagrant@vagrant.vm>
2018-11-28 09:19:23 +0000

    Added a BurgerBuilder component and basic css style for main section of layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
+//
 import React from 'react';
+
 import Aux from '../../hoc/Aux';
+
+import classes from './Layout.css';
 
 // Single root element achieve with hoc Aux
 const layout = (props) => (
 	<Aux>
 		<div>Toolbar, SideDrawer, Backdrop</div>
-		<main>
+		<main className={classes.Content}>
 			{props.children}
 		</main>
 	</Aux>
