687357b37e9fbb9fe331c641c08c04ab3ddb313f
vagrant <vagrant@vagrant.vm>
2018-12-12 09:18:58 +0000

    Added SideDrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,10 +7,13 @@ import classes from './Layout.css';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 // Single root element achieve with hoc Aux
 const layout = (props) => (
 	<Aux>
 		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>
 			{props.children}
 		</main>
