19c740750d5facc1f35e8fed20eca60ea3cf9cfd
Femchengdu <femiajayi705@gmail.com>
2018-12-15 14:57:45 +0000

    Added a hamburger icon

--- src/components/Navigation/SideDrawer/ToggleSide/ToggleSide.js
+++ src/components/Navigation/SideDrawer/ToggleSide/ToggleSide.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import classes from './ToggleSide.css';
+
 
 const toggleSide = (props) => (
-	<div onClick={props.clicked}>Menu</div>
+	<div className={classes.ToggleSide} onClick={props.clicked}>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
 );
 
 
