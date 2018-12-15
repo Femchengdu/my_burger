bd9ef2317ca3e4639061b0c0da133b6724997af5
Femchengdu <femiajayi705@gmail.com>
2018-12-15 17:45:41 +0000

    Added better performance by preventing unnecessary rerendering of the modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,42 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // Wrapping element not needed so use Auxilliary
 import Aux from '../../../hoc/Aux';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-	// Add logic before the return satement
-	const ingredientSummary = Object.keys(props.ingredients).map(
-		ingKey => {
-			// Wrap the return over multiple lines hence the ()
-			return (
-				<li key={ingKey}>
-					<span style={{textTransform: 'capitalize'}}>
-						{ingKey}
-					</span>: {props.ingredients[ingKey]}
-				</li>
-			);
-		}
-	);
+class OrderSummary extends Component {
+	// This should be a functional component
+	// It was changed to enable debugging
+	
+	// componentWillUpdate () {
+	// 	console.log('[OrderSummary] WillUpdate');
+	// }
 
-	return (
-		<Aux>
-			<h3>Your Order</h3>
-			<p>Your Burger includes the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-			<p>Continue Checkout?</p>
-			<Button type='Danger' clicked={props.cancel} >CANCEL</Button>
-			<Button type='Success' clicked={props.continue} >CONTINUE</Button>
-		</Aux>
-	);
-};
+	render () {
+		const ingredientSummary = Object.keys(this.props.ingredients).map(
+			ingKey => {
+				// Wrap the return over multiple lines hence the ()
+				return (
+					<li key={ingKey}>
+						<span style={{textTransform: 'capitalize'}}>
+							{ingKey}
+						</span>: {this.props.ingredients[ingKey]}
+					</li>
+				);
+			}
+		);
 
+		return (
+			<Aux>
+				<h3>Your Order</h3>
+				<p>Your Burger includes the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+				<p>Continue Checkout?</p>
+				<Button type='Danger' clicked={this.props.cancel} >CANCEL</Button>
+				<Button type='Success' clicked={this.props.continue} >CONTINUE</Button>
+			</Aux>
+		);
+	}
+}
 
-export default orderSummary;
+export default OrderSummary;
 
 
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 
@@ -6,21 +6,31 @@ import Aux from '../../../hoc/Aux';
 
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-	<Aux>
-		<Backdrop show={props.show} close={props.close} />
-		<div 
-		className={classes.Modal}
-		style={{
-			transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-			opacity: props.show ? '1' : '0'
-		}} >
-			{props.children}
-		</div>
-	</Aux>
-);
+class Modal extends Component {
+	shouldComponentUpdate (nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
 
+	componentWillUpdate () {
+		console.log('[Modal] Willupdate');
+	}
+	render () {
+		return (
+			<Aux>
+				<Backdrop show={this.props.show} close={this.props.close} />
+				<div 
+				className={classes.Modal}
+				style={{
+					transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+					opacity: this.props.show ? '1' : '0'
+				}} >
+					{this.props.children}
+				</div>
+			</Aux>
+		);
+	}
+} 
 
-export default modal;
+export default Modal;
 
 // Functional component receive props and return JSX
