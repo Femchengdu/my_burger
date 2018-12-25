5294e735edc2a301c3ca388483f61f5815a06d30
Femchengdu <femiajayi705@gmail.com>
2018-12-25 21:35:56 +0000

    Successfully fetched and parsed the previous orders from the server

--- src/containers/PrevOrders/PrevOrders.js
+++ src/containers/PrevOrders/PrevOrders.js
@@ -2,7 +2,35 @@ import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order';
 
+import axios from '../../axios_orders';
+
+import globalErrors from '../../hoc/globalErrors/globalErrors';
+
 class PrevOrders extends Component {
+
+	state = {
+		orders: [],
+		loading: true
+	}
+
+	componentDidMount () {
+		axios.get('/orders.json')
+			.then(result => {
+				let ordersArray = [];
+				for (let orderId in result.data) {
+					ordersArray.push({
+						...result.data[orderId],
+						id: orderId
+					});
+				}
+				console.log(ordersArray);
+				this.setState({loading: false, orders: ordersArray});
+			})
+			.catch(error => {
+				this.setState({loading: false});
+			})
+	}
+
 	render () {
 		return (
 			<div>
@@ -12,4 +40,4 @@ class PrevOrders extends Component {
 		);
 	}
 }
-export default PrevOrders;
+export default globalErrors(PrevOrders, axios);
