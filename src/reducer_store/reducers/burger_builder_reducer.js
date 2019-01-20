e79d93e030972fff4185146b10f273a43e669376
Femchengdu <femiajayi705@gmail.com>
2019-01-20 08:28:42 +0000

    Reset the price after each successful order

--- src/reducer_store/reducers/burger_builder_reducer.js
+++ src/reducer_store/reducers/burger_builder_reducer.js
@@ -38,6 +38,7 @@ const reducer = (state = initialState, action) => {
 			return {
 				...state,
 				ingredients: action.ingredients,
+				totalPrice: 2,
 				error: false
 			}
 		case action_types.set_ingeredients_fetch_error:
